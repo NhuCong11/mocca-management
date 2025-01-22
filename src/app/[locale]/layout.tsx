c6aec17705f8b4a6f17124b2f643ffba06454dc1
Công <yeusangtao96@gmail.com>
2025-01-22 15:31:58 +0700

    Add Mantine styles and create new globals.css for improved styling

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,5 +1,10 @@
 import type { Metadata } from 'next';
 
+import '@mantine/core/styles.css';
+import '@mantine/dates/styles.css';
+import '@mantine/charts/styles.css';
+import './globals.css';
+
 import { notFound } from 'next/navigation';
 import { getMessages } from 'next-intl/server';
 import { NextIntlClientProvider } from 'next-intl';
