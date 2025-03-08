ad7994b5b4a0e6b95d212cf95bd54ad73254e07c
Công <yeusangtao96@gmail.com>
2025-03-08 21:45:01 +0700

    Refactor AuthLayout and SignIn components to conditionally render UI based on token and login state; add Loading component

--- src/app/[locale]/auth/layout.tsx
+++ src/app/[locale]/auth/layout.tsx
@@ -4,22 +4,29 @@ import clsx from 'clsx';
 import styles from './layout.module.scss';
 import { MOCCA } from '@/constants';
 import { Link } from '@/i18n/routing';
+import { getLocalStorageItem } from '@/utils/localStorage';
+import { useAppSelector } from '@/lib/hooks';
 
 function AuthLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+  const token = JSON.parse(String(getLocalStorageItem('accessToken')));
+  const isLogin = useAppSelector((state) => state.auth.isLogin);
+
   return (
     <div className={clsx(styles['wrapper'])}>
-      <div className={clsx(styles['logo'])}>
-        <Link href={'/'}>
-          <Image
-            priority
-            width={145}
-            height={45}
-            alt={MOCCA}
-            src={'/images/logo-vip1.png'}
-            className={clsx(styles['logo__img'])}
-          />
-        </Link>
-      </div>
+      {!token && !isLogin && (
+        <div className={clsx(styles['logo'])}>
+          <Link href={'/'}>
+            <Image
+              priority
+              width={145}
+              height={45}
+              alt={MOCCA}
+              src={'/images/logo-vip1.png'}
+              className={clsx(styles['logo__img'])}
+            />
+          </Link>
+        </div>
+      )}
       <div className={clsx(styles['container'])}>{children}</div>
     </div>
   );
---
+++ src/app/[locale]/loading.tsx
@@ -0,0 +1,7 @@
+import LoadingStart from '@/share/Loading';
+
+function Loading() {
+  return <LoadingStart />;
+}
+
+export default Loading;
